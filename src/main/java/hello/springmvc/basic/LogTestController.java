1ebbf392bb9cbb690d64eddf9d624abc9ab9af39
jochongs <joch2712@naver.com>
2024-07-07 20:20:10 +0900

    Create log test

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,27 @@
+package hello.springmvc.basic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LogTestController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        // 운영 서버에 있을 필요가 없는 것은 출력하면 안 됨
+//        System.out.println();
+
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info("info log={}", name);
+        log.warn("warn log={}", name);
+        log.error("error log={}", name);
+
+        return "ok";
+    }
+}
