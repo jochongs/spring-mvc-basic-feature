89c3da74b81394d3657f38d258b07b5591cf8e80
jochongs <joch2712@naver.com>
2024-07-08 19:04:12 +0900

    Create RequestParamControllerV1

---
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -0,0 +1,25 @@
+package hello.springmvc.basic.request;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@Slf4j
+public class RequestParamController {
+
+    @RequestMapping("/request-parma-v1")
+    public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        log.info("username = {}", username);
+        log.info("age = {}", age);
+
+        response.getWriter().write("ok");
+    }
+}
